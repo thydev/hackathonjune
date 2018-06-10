db0c82404c049d225c1f76d36f23b18d9befdf88
Chanthy Ngin <nginchanthy@gmail.com>
2018-06-09 20:46:46 -0700

    connect to  mongodb cloud

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose'),
 
 module.exports = (() => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost:27017/ProductMasterSalesData');
+    // mongoose.connect('mongodb://localhost:27017/ProductMasterSalesData');
+
+    // Use your own username and passowrd
+    // mongoose.connect('mongodb+srv://username:password@cluster0-yi1u9.mongodb.net/ProductMasterSalesData?retryWrites=true');
+
     // create a variable that points to the models folder
     const models_path = path.join(__dirname, './../models');
     // read all of the files in the models_path and require (run) each of the javascript files
